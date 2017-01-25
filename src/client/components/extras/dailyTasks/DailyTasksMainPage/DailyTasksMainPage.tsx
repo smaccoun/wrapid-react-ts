ca71db535240b6b75dd184ede8832ccb487fb568
Steven MacCoun <theraccoun@gmail.com>
2017-01-25 12:43:39 -0800

    Started styling daily task header

--- src/client/components/extras/dailyTasks/DailyTasksMainPage/DailyTasksMainPage.tsx
+++ src/client/components/extras/dailyTasks/DailyTasksMainPage/DailyTasksMainPage.tsx
@@ -3,13 +3,23 @@ import * as ReactDOM from 'react-dom';
 
 const s = require('./style.css')
 
+import MainSchedule from '../../schedule/MainSchedule/MainSchedule'
+
+
 interface Props {
   model: any;
 }
 
 const DailyTasksMainPage = (props: Props) => {
   return(
-    <div>Daily tasks!</div>
+    <div className={s.container}>
+      <div className={s.headerInfo}>
+        <span className={s.date}>Monday May 25, 2016</span>
+        <span className={s.title}>Welcome, Steve</span>
+        <span className={s.production}>AMC's The Walking Dead</span>
+      </div>
+      <MainSchedule />
+    </div>
   )
 }
 
---
+++ src/client/components/extras/schedule/MainSchedule/MainSchedule.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const s = require('./style.css')
+
+const MainSchedule = () => {
+  return(
+    <div>Main Schedule!</div>
+  )
+}
+
+export default MainSchedule;

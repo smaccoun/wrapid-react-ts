fa5c7f94170d6b6121b2bd512a750f19d6120a60
Steven MacCoun <theraccoun@gmail.com>
2017-01-25 17:12:36 -0800

    Check in button styling

--- src/client/components/extras/schedule/MainSchedule/MainSchedule.tsx
+++ src/client/components/extras/schedule/MainSchedule/MainSchedule.tsx
@@ -4,13 +4,35 @@ import * as ReactDOM from 'react-dom';
 const s = require('./style.css')
 
 import DefaultPanel from '../../dailyTasks/panels/DefaultPanel/DefaultPanel'
+import PanelFooter from '../../dailyTasks/panels/PanelFooter/PanelFooter'
 
 
 const MainSchedule = () => {
   return(
-    <DefaultPanel headerItems={['Schedule', 'Monday']}>
-      <div>Schedule Item!</div>
+    <DefaultPanel headerItems={['Schedule', 'Monday May 25, 2017']}>
+      <ScheduleItem title={'Start Time'} startTm={'8:00 am'} />
+      <PanelFooter>
+        <div className={s.footer}>
+          <button className={s.checkinButton}>
+            <span>Check In</span>
+          </button>
+        </div>
+      </PanelFooter>
     </DefaultPanel>
+  )
+}
+
+interface SIProps {
+  title: string,
+  startTm: string
+}
+
+const ScheduleItem = (props: SIProps) => {
+  return(
+    <div className={s.itemContainer}>
+      <span className={s.title}>{props.title}</span>
+      <span className={s.startTm}>{props.startTm}</span>
+    </div>
   )
 }
 
